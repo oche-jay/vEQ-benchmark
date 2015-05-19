5172bbe6a9b9fa125aa4656750823922f71c0543
ooe <ooe@st-andrews.ac.uk>
2015-05-19 18:19:25 +0100

    added util

--- util/__init__.py
+++ util/__init__.py
@@ -3,6 +3,7 @@ def cleanResults(a_list,threshold=None):
     '''
     Take a list probabbly the results of a sqllite query
     Flatten it and then remove any values lower than zero
+    Maybe
     '''
     a_list = list(sum(a_list,())) #flatten list
     a_list[:] = [x for x in a_list if x > 0] #retain values if > 0
